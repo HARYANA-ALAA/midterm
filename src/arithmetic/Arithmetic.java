09b1e59c58e9d6adc2e1a8ee736b527f89491339
HARYANA-ALAA <ankit6@sheridancollege.ca>
2023-07-10 14:14:01 -0400

    Branch_Version_2

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -7,7 +7,6 @@ package arithmetic;
 
 
 import java.util.Scanner;
-import static java.time.Clock.system;
 
 /** This class calls the method to perform 
  * arithmetic operations based on user input
@@ -15,22 +14,73 @@ import static java.time.Clock.system;
  * @author sivagamasrinivasan
  * 
  */
+public class ArithmeticCalculator 
+{
+    public double calculate(double x, double y, ArithmeticOperation operation) {
+        switch (operation) {
+            case PLUS:
+                return x + y;
+            case MINUS:
+                return x - y;
+            case TIMES:
+                return x * y;
+            case DIVIDE:
+                return x / y;
+            default:
+                throw new AssertionError("Unknown operation: " + operation);
+        }
+    }
+}
+
+enum ArithmeticOperation {
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE
+}
+class ArithmeticInput {
+    private double x;
+    private double y;
+
+    public void setNumbers(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public ArithmeticOperation getOperationFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter arithmetic operation to perform (PLUS, MINUS, TIMES, DIVIDE): ");
+        String input = scanner.next().toUpperCase();
+        return ArithmeticOperation.valueOf(input);
+    }
+}
+
+
+
+
+
 public class Arithmetic 
 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+    public static void main(String[] args) {
+        ArithmeticCalculator calculator = new ArithmeticCalculator();
+        ArithmeticInput input = new ArithmeticInput();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+        input.setNumbers(x, y);
+
+        ArithmeticOperation operation = input.getOperationFromUser();
+
+        double result = calculator.calculate(x, y, operation);
+        System.out.println("Result: " + result);
     }
 }
+
+
 
